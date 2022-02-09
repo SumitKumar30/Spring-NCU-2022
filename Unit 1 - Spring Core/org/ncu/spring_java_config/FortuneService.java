5bcec0f14643285cfd01185820c64964ff0acc95
Sumit Kumar <mail.sumitkumar30@gmail.com>
2022-02-09 13:13:45 +0530

    Delete Unit 1 - Spring Core/org directory

--- Unit 1 - Spring Core/org/ncu/spring_java_config/FortuneService.java
+++
@@ -1,6 +0,0 @@
-package org.ncu.spring_java_config;
-
-// Helper Service
-public interface FortuneService {
-	public String getFortune();
-}
--- Unit 1 - Spring Core/org/ncu/spring_java_config/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package org.ncu.spring_java_config;
-
-public interface GreetingService {
-	
-	public void greet(String name);
-	
-	public String getDailyFortune();
-
-}
